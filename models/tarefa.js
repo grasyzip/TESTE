43dac2c3a01ca33afc903362401aec8d2e4b9533
Grasielly R <159390727+grasyzip@users.noreply.github.com>
2026-04-22 20:45:38 -0300

    Update tarefa.js

--- models/tarefa.js
+++ models/tarefa.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
+
 const schemaTarefa = new mongoose.Schema({
-    descricao: {
-        required: true,
-        type: String
-    },
-    statusRealizada: {
-        required: true,
-        type: Boolean
-    },
-},
-    {
-        versionKey: false
-    }
-)
-module.exports = mongoose.model('Tarefa', schemaTarefa)
+  descricao: {
+    required: true,
+    type: String
+  },
+  statusRealizada: {
+    required: true,
+    type: Boolean
+  }
+}, {
+  versionKey: false  // Remove o __v do MongoDB
+});
+
+// Força o nome da coleção como 'tarefas' (sem plural automático)
+module.exports = mongoose.model('Tarefa', schemaTarefa, 'tarefas');
